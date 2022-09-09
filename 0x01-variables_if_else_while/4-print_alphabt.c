d012cdfdbbe32c5bc7fa3df54c9564e2e592e2db
shuntel <111053515+shuntel@users.noreply.github.com>
2022-09-09 16:57:17 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,11 @@ int main(void)
 	char a;
 	for (a ='a' ; a <= 'z' ; a++)
 	{
-		if (a == 'q' || a== 'e')
+		if (a == 'q' || a == 'e')
 		{
 			continue;
 		}
 		putchar(a);
-	}putchar('\n');
+	} putchar('\n');
 	return (0);
 }
