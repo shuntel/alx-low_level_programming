da6bed48990b89730067f1897d9b51270e791e45
shuntel <111053515+shuntel@users.noreply.github.com>
2022-09-15 00:08:55 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,15 @@
 #include"main.h"
 
 /**
- * print_alphabet_x10 - function that will display the alphabet 10 times
- * from a - z
+ * print_alphabet - utilizes on the _putchar function to print
+ *                 the alphabet a - z
 */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
-	int line, ch;
+	int ch;
 
-	for (line = 0; line <= 9; ++line)
-	{
-		for (ch = 'a'; ch <= 'z'; ++ch)
-			_putchar(ch);
-		_putchar('\n');
-	}
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
 }
