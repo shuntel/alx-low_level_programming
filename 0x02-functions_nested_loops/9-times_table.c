fca2478fdd2509dc1a58d4017a9e91462afe3436
shuntel <111053515+shuntel@users.noreply.github.com>
2022-09-15 01:11:32 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
+##include "main.h"
 
 /**
- * numLength - returns the length of string
+ * times_table - prints the 9 times table
  *
- * @num: operand number
+ * Example Table
+ * 0, 0, 0, 0, ..
+ * 0, 1, 2, 3, ..
  *
- * Return: number of digits
 */
 
 void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; ++num)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		_putchar(48);
+		for (mult = 1; mult <= 9; ++mult)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -26,9 +26,9 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0');
+				_putchar((prod / 10) + 48); /get the first digit/
 
-			_putchar((prod % 10) + '0');
+			_putchar((prod % 10) + 48); /get the second digit/
 		}
 		_putchar('\n');
 	}
