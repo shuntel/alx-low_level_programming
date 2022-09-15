b0f49af3292288d7da96905d1966822229e9ad73
shuntel <111053515+shuntel@users.noreply.github.com>
2022-09-15 01:09:51 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,65 +8,28 @@
  * Return: number of digits
 */
 
-int numLength(int num)
+void times_table(void)
 {
-	int length = 0;
+	int num, mult, prod;
 
-	if (!num)
-		return (1);
-
-	while (num)
+	for (num = 0; num <= 9; num++)
 	{
-		num = num / 10;
-		length += 1;
-	}
+		_putchar('0');
 
-	return (length);
-}
-
-/**
- * main - Entry point
- *
- * Description: prints the first 98 Fibonacci numbers
- *	starting with 1 and 2 followed by a new line
- *
- *	Solution was copied from Nobert Patrick
- *	Wise, github handle: Trikcode
- *
- * Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-	int count, initial0;
-	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
-
-	for (count = 1; count <= 98; ++count)
-	{
-		if (f1o > 0)
-			printf("%lu", f1o);
-		initial0 = numLength(mx) - 1 - numLength(f1);
-
-		while (f1o > 0 && initial0 > 0)
+		for (mult = 1; mult <= 9; mult++)
 		{
-			printf("%d", 0);
-			--initial0;
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
-
-		printf("%lu", f1);
-
-		sum = (f1 + f2) % mx;
-		sumo = f1o + f2o + (f1 + f2) / mx;
-		f1 = f2;
-		f1o = f2o;
-		f2 = sum;
-		f2o = sumo;
-
-		if (count != 98)
-			printf(", ");
-		else
-			printf("\n");
+		_putchar('\n');
 	}
-
-	return (0);
 }
