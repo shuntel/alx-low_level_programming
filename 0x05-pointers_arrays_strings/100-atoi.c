d8e0b4eb518c12471f726af1bde7a7e724c53658
shuntel <111053515+shuntel@users.noreply.github.com>
2022-09-19 17:53:06 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,16 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
-
  * _atoi - Entry point
-
  * @s: input
-
  * Return: Always 0 (Success)
-
  */
-
 int _atoi(char *s)
-
 {
 
-
-
 	return (0);
-
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
