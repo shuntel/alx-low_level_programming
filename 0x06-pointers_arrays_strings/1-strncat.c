d153054071939c56ad477353286940ebc01613c2
shuntel <111053515+shuntel@users.noreply.github.com>
2022-09-21 11:36:29 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,51 +1,32 @@
-#include "holberton.h"
-
-
+#include "main.h"
 
 /**
-
- * _strncat - a function that concatenates two strings
-
- * @dest: copy to
-
- * @src: copy from
-
- * @n: input of max bytes to be used
-
- * Return: Always 0 (Success)
-
- */
+ * _strncat - a function that concatenates two strings.
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input
+ * @n: most number of bytes from @src
+ *
+ * Return: @dest
+*/
 
 char *_strncat(char *dest, char *src, int n)
-
 {
+	int c, i;
 
-	int i, co;
+	c = 0;
 
+	/*find size of dest array*/
+	while (dest[c])
+		c++;
 
-
-	for (i = 0; dest[i] != '\0'; i++)
-
-	{
-
-	}
-
-
-
-	for (co = 0; co < n; co++)
-
-	{
-
-		dest[i + co] = src[co];
-
-		if (src[co] == '\0')
-
-			co = n;
-
-	}
-
-
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
 
 	return (dest);
-
-}
