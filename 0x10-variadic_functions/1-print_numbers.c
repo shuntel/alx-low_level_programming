989c3cdea83c36c89ab3ca57f495d27323f516fb
shuntel <111053515+shuntel@users.noreply.github.com>
2022-10-13 20:41:49 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,23 +1,32 @@
 #include "variadic_functions.h"
-
-
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
-
- * main - check the code
-
- *
-
- * Return: Always 0.
-
+ * print_numbers - print numbers
+ * @separator: item to use ot separate numbers
+ * @n: number of argument to sum
+ * Return: sum of all argument
  */
 
-int main(void)
 
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i = 0;
+	va_list ap;
 
-    print_numbers(", ", 4, 0, 98, -1024, 402);
+	va_start(ap, n);
 
-    return (0);
+	if (!separator)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d%s", va_arg(ap, int), i != (n - 1) ? separator : "");
+	}
+
+
+	va_end(ap);
+	putchar('\n');
 
 }
