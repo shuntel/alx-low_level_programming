322306374427c930694104dd77e8e16f4293ab1c
shuntel <111053515+shuntel@users.noreply.github.com>
2022-10-28 14:07:39 +0100

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,35 +1,36 @@
-#include "holberton.h"
-
-
+#include  "main.h"
+/**
+  *getp - returns the value of x to the power of y.
+  *@x: number.
+  *@y: power.
+  *
+  *Return: x to the power of y.
+  */
+int getp(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (x * getp(x, y - 1));
+}
 
 /**
-
- * set_bit - A function that sets a bit at given index to 1
-
- * @n: The number to set bit in
-
- * @index: The index to set bit at
-
- * Return: 1 if it worked, or -1 on error
-
- */
-
+  *set_bit - stes the value of a bit to one at a given index.
+  *@n: pointer to number.
+  *@index: index of bit.
+  *
+  *Return: 1 if it worked, -1 if it did not.
+  */
 int set_bit(unsigned long int *n, unsigned int index)
-
 {
-
-	unsigned long int max = 0x01;
-
-
-
-	max <<= index;
-
-	if (max == 0)
-
+	if (n && index < 64)
+	{
+		*n += getp(2, index);
+	}
+	else
+	{
 		return (-1);
-
-	*n |= max;
-
+	}
 	return (1);
-
 }
